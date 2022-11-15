8f11ca0ef0d56fac4467b60ac4822e7048f62596
toomspawt <anhvnguyen22@gmail.com>
2022-11-14 22:09:10 -0500

    add css to bookfinder

--- frontend/bookfinder/bookfinder.js
+++ frontend/bookfinder/bookfinder.js
@@ -25,17 +25,18 @@ displayBook = (item) => {
     divBook = document.createElement("div");
     divBook.classList.add("book");
     divBook.innerHTML = `
-        <a href="${item.volumeInfo.canonicalVolumeLink}">${item.volumeInfo.title}</a>
-        <br>
+        <a href="${item.volumeInfo.canonicalVolumeLink}" target="_blank">${item.volumeInfo.title}</a>
         <img src="${item.volumeInfo.imageLinks.smallThumbnail}" alt="${item.volumeInfo.title}">
         <div class="book-info">
-            <strong>Author: </strong> ${item.volumeInfo.authors ? item.volumeInfo.authors[0] : "N/A"}
-        </div>
-        <div class="book-info">
-            <strong>Publisher: </strong> ${item.volumeInfo.publisher ? item.volumeInfo.publisher : "N/A"}
-        </div>
-        <div class="book-info">
-            <strong>Published: </strong> ${item.volumeInfo.publishedDate ? item.volumeInfo.publishedDate : "N/A"}
+            <div>
+                <strong>Author: </strong> ${item.volumeInfo.authors ? item.volumeInfo.authors[0] : "N/A"}
+            </div>
+            <div>
+                <strong>Publisher: </strong> ${item.volumeInfo.publisher ? item.volumeInfo.publisher : "N/A"}
+            </div>
+            <div>
+                <strong>Published: </strong> ${item.volumeInfo.publishedDate ? item.volumeInfo.publishedDate : "N/A"}
+            </div>
         </div>
     `
     return divBook;
